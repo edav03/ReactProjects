6e524aba2e28fe528f4a405a1d7b69da66411a81
Edison <david.alcocer.2003@hotmail.com>
2021-08-07 19:35:55 +0200

    Mensage

---
+++ CursoApp/1prueba/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Custom.css'
+
+function App() {
+  return(
+    <div className="outerdive">
+      <div className="innerdive">This is something</div>
+    </div>
+  )
+}
+
+export default App;
