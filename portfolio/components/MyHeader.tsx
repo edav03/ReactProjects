c0bac231ea86e170585640ec423dbef76d99d614
Edison <edavalc03@hotmail.com>
2022-04-21 18:06:01 +0200

    Little changes

--- portfolio/components/MyHeader.tsx
+++ portfolio/components/MyHeader.tsx
@@ -1,60 +1,56 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-const MyHeader = ({title} : {title?: string }) => {
-  return(
-    <header className={styles.header}>
-          <h1 className={styles.title}>
-            <Image
-              src={"/imgs/react.png"}
-              height={40}
-              width={40}
-              alt="react"
-            />
-            {' '}
-            {title}
-            {' | HOME'}
-          </h1>
-          <h1 className={styles.description}>
-            <a href="https://github.com/edav03">
-              GitHub
-              <Image
-               src={"/imgs/github.png"}
-               height={40}
-               width={40}
-               alt="github"
-              />
-            </a>
-          </h1>
-          <h1 className={styles.description}>
-            <Link href="/posts/projects">
-              <a>
-              {' '}
-              Projects
-              <Image
-               src={"/imgs/terminal.png"}
-               height={40}
-               width={40}
-               alt="edav03"
-              />
-              </a>
-            </Link>
-          </h1>
-          <h1 className={styles.description}>
-            <a href="">
-              {' '}
-              Resume
-              <Image
-               src="/imgs/resume.png"
-               height={40}
-               width={40}
-               alt="github"
-              />
-            </a>
-          </h1>
-        </header>
-  )
+interface IMyHeader {
+  userName: string;
+  currentPage: string;
 }
 
-export default MyHeader
+const MyHeader = (args: IMyHeader) => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        <Link href="/">
+          <a className={styles.descriptionHome}>
+            <Image src={"/imgs/react.png"} height={40} width={40} alt="react" />
+            {`${args.userName} | ${args.currentPage}`}
+          </a>
+        </Link>
+      </h1>
+
+      <h1 className={styles.description}>
+
+        <Link href="/projects">
+          <a className={styles.descriptionLinks}>
+            Projects
+            <Image
+              src={"/imgs/terminal.png"}
+              height={40}
+              width={40}
+              alt="edav03"
+            />
+          </a>
+        </Link>
+
+        <a className={styles.descriptionLinks}>
+          Resume
+          <Image src="/imgs/resume.png" height={40} width={40} alt="github" />
+        </a>
+
+        <a
+          className={styles.descriptionLinks}
+          href="https://github.com/edav03"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+          <Image src={"/imgs/github.png"} height={40} width={40} alt="github" />
+        </a>
+
+      </h1>
+    </header>
+  );
+};
+
+export default MyHeader;
--- portfolio/pages/projects.tsx
+++ portfolio/pages/projects.tsx
@@ -2,16 +2,11 @@ import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import MyHead from "../components/MyHead";
 import MyHeader from "../components/MyHeader";
-import InBody from "../components/InBody";
 import MyFooter from "../components/MyFooter";
 
-const HomeBody = () => {
-  return <></>;
-};
-
-const Home = () => {
+export const Projects = () => {
   const [userName, setUserName] = useState<string>("Edison");
-  const [currentPage, setCurrentPage] = useState<string>("HOME");
+  const [currentPage, setCurrentPage] = useState<string>("Projects");
 
   return (
     <div>
@@ -19,14 +14,10 @@ const Home = () => {
 
       <main className={styles.main}>
         <MyHeader userName={userName} currentPage={currentPage} />
-
-        <HomeBody />
-        <InBody />
-
         <MyFooter />
       </main>
     </div>
   );
 };
 
-export default Home;
+export default Projects;
